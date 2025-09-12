6753744fb9510eb17106404c4d9b7071e55cdf21
Lyle Zhyla <elyl.patalod16@gmail.com>
2025-09-12 21:06:12 +0800

    Initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,72 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+// src/pages/Home.tsx
+import React from "react";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+} from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
+      {/* 🔹 Header */}
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>My Ionic Grid App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+
+      {/* 🔹 Content with Grid */}
+      <IonContent className="ion-padding">
+        <IonGrid>
+          {/* Row 1 */}
+          <IonRow>
+            <IonCol size="6">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Column 1</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  This is the first column.
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol size="6">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Column 2</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  This is the second column.
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+
+          {/* Row 2 */}
+          <IonRow>
+            <IonCol size="12">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Full Width</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  This takes up the whole row (12 columns).
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
